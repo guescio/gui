76b26d52f24c564949c58b3e57f91fe595516e19
a-bombarda <andrea.bombarda@unibg.it>
2020-04-20 16:16:46 +0200

    First test cases

---
+++ gui/tests/mvm_basics.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import os, sys, yaml
+
+from communication.fake_esp32serial import FakeESP32Serial
+from alarm_handler import AlarmHandler
+
+
+base_dir = os.environ['MVMGUI_BASEDIR']
+settings_file = os.path.join(base_dir, 'gui/default_settings.yaml')
+with open(settings_file) as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+
+if __name__ == "__main__":
+    print('Config:', yaml.dump(config), sep='\n')
