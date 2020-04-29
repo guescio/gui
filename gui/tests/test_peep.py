43e66258a0b2dddffdceb1c976aac65d32aeb729
Carmelo Pellegrino <carmelo.pellegrino@gmail.com>
2020-04-29 13:58:22 +0200

    update variables used in the test
    t5 -> phase_start["restart"]
    t0 -> t_cycle_start

--- gui/tests/test_peep.py
+++ gui/tests/test_peep.py
@@ -18,13 +18,13 @@ def test_peepReset(qtbot):
     p = PEEP()
 
     # Set a negative value for the t5 parameter, so the time t is obiously greater than t5
-    t5 = p.t5
-    p.t5 = -1
-    t0 = p.t0
+    t5 = p.phase_start["restart"]
+    p.phase_start["restart"] = -1
+    t0 = p.t_cycle_start
 
     # Force the flow
     p.flow()
 
-    assert p.t0 != t0
+    assert p.t_cycle_start != t0
 
-    p.t5 = t5
+    p.phase_start["restart"] = phase_start["restart"]
